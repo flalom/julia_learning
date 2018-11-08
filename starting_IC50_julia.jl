6b7b1d47efea1d0cc769388f264ce518d69f196f
flalom <esum88@yahoo.com>
2018-11-08 22:17:24 +0100

    all the files hfgfg

--- starting_IC50_julia.jl
+++ starting_IC50_julia.jl
@@ -6,4 +6,7 @@ using Queryverse
 println("Function for calculation of the linear curve associated with the data points")
 data = readlines("pippo.txt")
 
-data = Queryverse.readtable("pippo.txt")
+data = load("2018-10-06__Paper_table.xlsx", "Jennifer Keiser Author   Full ") |> DataFrame
+
+pipo = data |> @filter(_.year==2018)
+save("gigio.csv", delim=';', pipo)
