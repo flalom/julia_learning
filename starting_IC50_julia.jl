c8537333c1e6b36d0e3a488343da1ea5e072ae42
Flavio <Flavio.lombardo@unibas.ch>
2018-11-09 16:19:43 +0100

    Interesting beginning a possible fnuction for the calculation of IC50s

--- starting_IC50_julia.jl
+++ starting_IC50_julia.jl
@@ -4,9 +4,20 @@ using Pkg
 using Queryverse
 
 println("Function for calculation of the linear curve associated with the data points")
+#I would like to make a function that is asking the user to save the data
 data = readlines("pippo.txt")
-
 data = load("2018-10-06__Paper_table.xlsx", "Jennifer Keiser Author   Full ") |> DataFrame
-
 pipo = data |> @filter(_.year==2018)
 save("gigio.csv", delim=';', pipo)
+
+using Queryverse
+#I will make a function that is asking for the DataFrame of the effect
+calling_data_frame = function()
+    #entering the file name and all the basic thigs that I would need from this function
+    println("Please enter the file name, complete of extension")
+    file_toload = readline(stdin)
+
+    #Now staring playing with the file just loaded
+    #Loading the dataset now and globally saving the dataset
+    global data_df = load(file_toload) |> DataFrame
+end
