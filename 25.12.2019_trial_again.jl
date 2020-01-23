9629b169a1ea56b55eee2d8caac6936f5bad4f90
flalom <esum88@yahoo.com>
2020-01-23 12:25:14 +0100

    Corrected typos in the code

--- 25.12.2019_trial_again.jl
+++ 25.12.2019_trial_again.jl
@@ -1,12 +1,12 @@
-function ripetitore(num=>=>Int, rep=>=>Int, text=>=>AbstractString) =>=> Any
-    for i in 1=>num
+function ripetitore(num::Int, rep::Int, text::AbstractString) :: Any
+    for i in 1::num
         println("  "^(i+1), text^rep,  "  "^(i+1), text^rep)
     end
 end
 
 #ripetitore(15, 2, "<3 ")
 
-function reverser(str=>=>AbstractString)
+function reverser(str::AbstractString)
     collection = collect(str)
     for char in reverse(collection)
         if char!== "m"
@@ -18,8 +18,8 @@ function reverser(str=>=>AbstractString)
 end
 
 #reverser("pillmmmmaaa")
-function dna_complementary(sequence_dna=>=>AbstractString)
-    bases = Dict("a" => "t", "c" => "g", "g"=>"c", "t"=>"a")
+function dna_complementary(sequence_dna::AbstractString)
+    bases = Dict("a" :: "t", "c" :: "g", "g"::"c", "t"::"a")
     complementary_seq = []
     sequence_low = lowercase(sequence_dna)
     for el in sequence_low
@@ -36,7 +36,7 @@ end
 dna_complementary("gccttgggaatttAAAAAAAAAAAAAAAAAAGGGGGGGGGGGGGGGGGGGG")
 
 function traslatore_dna_in_rna(string_dna :: AbstractString) ::AbstractString
-    bases = Dict("a" => "t", "c" => "g", "g"=>"c", "t"=>"T")
+    bases = Dict("a" :: "t", "c" :: "g", "g"::"c", "t"::"T")
     println("You provided a sequence of ", length(string_dna))
     sequence_low_rna = lowercase(string_dna)
     complementary_seq = []
@@ -51,22 +51,22 @@ traslatore_dna_in_rna("gccttgggaattttgcAAAAAAAAAAAAAAAAAAGGGGGGGGGGGGGGGGGGGGTTT
 function translator_from_dna(rna_seq :: AbstractString)
     traslation = []
     table = Dict(
-        "AUA"=>"I", "AUC"=>"I", "AUU"=>"I", "AUG"=>"M", 
-        "ACA"=>"U", "ACC"=>"T", "ACG"=>"T", "ACU"=>"T", 
-        "AAC"=>"N", "AAU"=>"N", "AAA"=>"K", "AAG"=>"K", 
-        "AGC"=>"S", "AGU"=>"S", "AGA"=>"R", "AGG"=>"R",                  
-        "CUA"=>"L", "CUC"=>"L", "CUG"=>"L", "CUU"=>"L", 
-        "CCA"=>"P", "CCC"=>"P", "CCG"=>"P", "CCU"=>"P", 
-        "CAC"=>"H", "CAU"=>"H", "CAA"=>"Q", "CAG"=>"Q", 
-        "CGA"=>"R", "CGC"=>"R", "CGG"=>"R", "CGU"=>"R", 
-        "GUA"=>"V", "GUC"=>"V", "GUG"=>"V", "GUU"=>"V", 
-        "GCA"=>"A", "GCC"=>"A", "GCG"=>"A", "GCU"=>"A", 
-        "GAC"=>"D", "GAU"=>"D", "GAA"=>"E", "GAG"=>"E", 
-        "GGA"=>"G", "GGC"=>"G", "GGG"=>"G", "GGU"=>"G", 
-        "UCA"=>"S", "UCC"=>"S", "UCG"=>"S", "UCU"=>"S", 
-        "UUC"=>"F", "UUU"=>"F", "UUA"=>"L", "UUG"=>"L", 
-        "UAC"=>"Y", "UAU"=>"Y", "UAA"=>"_", "UAG"=>"_", 
-        "UGC"=>"C", "UGU"=>"C", "UGA"=>"_", "UGG"=>"W" )
+        "AUA"::"I", "AUC"::"I", "AUU"::"I", "AUG"::"M", 
+        "ACA"::"U", "ACC"::"T", "ACG"::"T", "ACU"::"T", 
+        "AAC"::"N", "AAU"::"N", "AAA"::"K", "AAG"::"K", 
+        "AGC"::"S", "AGU"::"S", "AGA"::"R", "AGG"::"R",                  
+        "CUA"::"L", "CUC"::"L", "CUG"::"L", "CUU"::"L", 
+        "CCA"::"P", "CCC"::"P", "CCG"::"P", "CCU"::"P", 
+        "CAC"::"H", "CAU"::"H", "CAA"::"Q", "CAG"::"Q", 
+        "CGA"::"R", "CGC"::"R", "CGG"::"R", "CGU"::"R", 
+        "GUA"::"V", "GUC"::"V", "GUG"::"V", "GUU"::"V", 
+        "GCA"::"A", "GCC"::"A", "GCG"::"A", "GCU"::"A", 
+        "GAC"::"D", "GAU"::"D", "GAA"::"E", "GAG"::"E", 
+        "GGA"::"G", "GGC"::"G", "GGG"::"G", "GGU"::"G", 
+        "UCA"::"S", "UCC"::"S", "UCG"::"S", "UCU"::"S", 
+        "UUC"::"F", "UUU"::"F", "UUA"::"L", "UUG"::"L", 
+        "UAC"::"Y", "UAU"::"Y", "UAA"::"_", "UAG"::"_", 
+        "UGC"::"C", "UGU"::"C", "UGA"::"_", "UGG"::"W" )
     
     start_translation = findfirst("AUG", uppercase(rna_seq))[2]
     triplets = 2
