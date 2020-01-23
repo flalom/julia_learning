70934e9fe666a77cfa2f3331ab61dc33d987de49
flalom <flavio.lombardo@unibas.ch>
2020-01-23 11:14:01 +0100

    Adding additional training file in Julia lang

--- 25.12.2019_trial_again.jl
+++ 25.12.2019_trial_again.jl
@@ -36,7 +36,7 @@ end
 dna_complementary("gccttgggaatttAAAAAAAAAAAAAAAAAAGGGGGGGGGGGGGGGGGGGG")
 
 function traslatore_dna_in_rna(string_dna :: AbstractString) ::AbstractString
-    bases = Dict("a" => "t", "c" => "g", "g"=>"c", "t"=>"u")
+    bases = Dict("a" => "t", "c" => "g", "g"=>"c", "t"=>"T")
     println("You provided a sequence of ", length(string_dna))
     sequence_low_rna = lowercase(string_dna)
     complementary_seq = []
@@ -51,32 +51,34 @@ traslatore_dna_in_rna("gccttgggaattttgcAAAAAAAAAAAAAAAAAAGGGGGGGGGGGGGGGGGGGGTTT
 function translator_from_dna(rna_seq :: AbstractString)
     traslation = []
     table = Dict(
-        "ATA"=>"I", "ATC"=>"I", "ATT"=>"I", "ATG"=>"M", 
-        "ACA"=>"T", "ACC"=>"T", "ACG"=>"T", "ACT"=>"T", 
-        "AAC"=>"N", "AAT"=>"N", "AAA"=>"K", "AAG"=>"K", 
-        "AGC"=>"S", "AGT"=>"S", "AGA"=>"R", "AGG"=>"R",                  
-        "CTA"=>"L", "CTC"=>"L", "CTG"=>"L", "CTT"=>"L", 
-        "CCA"=>"P", "CCC"=>"P", "CCG"=>"P", "CCT"=>"P", 
-        "CAC"=>"H", "CAT"=>"H", "CAA"=>"Q", "CAG"=>"Q", 
-        "CGA"=>"R", "CGC"=>"R", "CGG"=>"R", "CGT"=>"R", 
-        "GTA"=>"V", "GTC"=>"V", "GTG"=>"V", "GTT"=>"V", 
-        "GCA"=>"A", "GCC"=>"A", "GCG"=>"A", "GCT"=>"A", 
-        "GAC"=>"D", "GAT"=>"D", "GAA"=>"E", "GAG"=>"E", 
-        "GGA"=>"G", "GGC"=>"G", "GGG"=>"G", "GGT"=>"G", 
-        "TCA"=>"S", "TCC"=>"S", "TCG"=>"S", "TCT"=>"S", 
-        "TTC"=>"F", "TTT"=>"F", "TTA"=>"L", "TTG"=>"L", 
-        "TAC"=>"Y", "TAT"=>"Y", "TAA"=>"_", "TAG"=>"_", 
-        "TGC"=>"C", "TGT"=>"C", "TGA"=>"_", "TGG"=>"W" )
+        "AUA"=>"I", "AUC"=>"I", "AUU"=>"I", "AUG"=>"M", 
+        "ACA"=>"U", "ACC"=>"T", "ACG"=>"T", "ACU"=>"T", 
+        "AAC"=>"N", "AAU"=>"N", "AAA"=>"K", "AAG"=>"K", 
+        "AGC"=>"S", "AGU"=>"S", "AGA"=>"R", "AGG"=>"R",                  
+        "CUA"=>"L", "CUC"=>"L", "CUG"=>"L", "CUU"=>"L", 
+        "CCA"=>"P", "CCC"=>"P", "CCG"=>"P", "CCU"=>"P", 
+        "CAC"=>"H", "CAU"=>"H", "CAA"=>"Q", "CAG"=>"Q", 
+        "CGA"=>"R", "CGC"=>"R", "CGG"=>"R", "CGU"=>"R", 
+        "GUA"=>"V", "GUC"=>"V", "GUG"=>"V", "GUU"=>"V", 
+        "GCA"=>"A", "GCC"=>"A", "GCG"=>"A", "GCU"=>"A", 
+        "GAC"=>"D", "GAU"=>"D", "GAA"=>"E", "GAG"=>"E", 
+        "GGA"=>"G", "GGC"=>"G", "GGG"=>"G", "GGU"=>"G", 
+        "UCA"=>"S", "UCC"=>"S", "UCG"=>"S", "UCU"=>"S", 
+        "UUC"=>"F", "UUU"=>"F", "UUA"=>"L", "UUG"=>"L", 
+        "UAC"=>"Y", "UAU"=>"Y", "UAA"=>"_", "UAG"=>"_", 
+        "UGC"=>"C", "UGU"=>"C", "UGA"=>"_", "UGG"=>"W" )
     
-    start_translation = findfirst("TAA", uppercase(rna_seq))[1]
-    triplets = 3
+    start_translation = findfirst("AUG", uppercase(rna_seq))[2]
+    triplets = 2
     for (num, sequence) in enumerate(start_translation:length(rna_seq))
-        if num % 3 == 0
-            triplents = rna_seq[sequence:sequence + 2]
+        if num % 3 == 0 && num < length(rna_seq)
+            triplents = rna_seq[sequence:sequence + triplets]
+            println(triplents)
             append!(traslation, table[triplents])
         end
     end
     return join(traslation, "")
 end
 
-translator_from_dna("TAATGTACGGGTCGTCTGGTATGGGT")
+translator_from_dna("UUUAUGGCCAUGGCGCCCAGAACUGAGAUCAAUAGUACCCGUAUUAACGGGUGAUUCACAUGUAAA")
+#MAMAPRTEINSTRING
