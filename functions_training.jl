1fb04a7c0bc27489106d06d4119fef012e8f3ac4
Flavio <flavio.lombardo@unibas.ch>
2020-01-25 17:31:25 +0100

    Update functions_training.jl
    Integrating version 2 of the function by adding the argument from the command line input.

--- functions_training.jl
+++ functions_training.jl
@@ -53,6 +53,7 @@ end
 
 #traslatore_dna_in_rna("gccttgggaattttgcAAAAAAAAAAAAAAAAAAGGGGGGGGGGGGGGGGGGGGTTTTTTTTTTTTTTTTT")
 
+
 function translator_from_rna(rna_seq :: AbstractString)
     """This function provided a RNA sequence will generate the protein sequence starting from the first encountered AUG"""
     traslation = []
@@ -86,4 +87,41 @@ function translator_from_rna(rna_seq :: AbstractString)
     return join(traslation, "")
 end
 
-translator_from_rna("UUUAUGGCCAUGGCGCCCAGAACUGAGAUCAAUAGUACCCGUAUUAACGGGUGAUUCACAUGUAAA")
+#translator_from_rna("UUUAUGGCCAUGGCGCCCAGAACUGAGAUCAAUAGUACCCGUAUUAACGGGUGAUUCACAUGUAAA")
+
+function translator_from_rna_v2((ARGS[1]) :: AbstractString)
+    """This function provided a RNA sequence will generate the protein sequence starting from the first encountered AUG.
+    However, in contrast to the previous function this one takes the function argument from the command line"""
+    
+    #This will throw an error in case the lenght of the argument is not correct
+    length(ARGS) == 1 || error("I need one command line argument")
+    traslation = []
+    table = Dict(
+        "AUA"::"I", "AUC"::"I", "AUU"::"I", "AUG"::"M", 
+        "ACA"::"U", "ACC"::"T", "ACG"::"T", "ACU"::"T", 
+        "AAC"::"N", "AAU"::"N", "AAA"::"K", "AAG"::"K", 
+        "AGC"::"S", "AGU"::"S", "AGA"::"R", "AGG"::"R",                  
+        "CUA"::"L", "CUC"::"L", "CUG"::"L", "CUU"::"L", 
+        "CCA"::"P", "CCC"::"P", "CCG"::"P", "CCU"::"P", 
+        "CAC"::"H", "CAU"::"H", "CAA"::"Q", "CAG"::"Q", 
+        "CGA"::"R", "CGC"::"R", "CGG"::"R", "CGU"::"R", 
+        "GUA"::"V", "GUC"::"V", "GUG"::"V", "GUU"::"V", 
+        "GCA"::"A", "GCC"::"A", "GCG"::"A", "GCU"::"A", 
+        "GAC"::"D", "GAU"::"D", "GAA"::"E", "GAG"::"E", 
+        "GGA"::"G", "GGC"::"G", "GGG"::"G", "GGU"::"G", 
+        "UCA"::"S", "UCC"::"S", "UCG"::"S", "UCU"::"S", 
+        "UUC"::"F", "UUU"::"F", "UUA"::"L", "UUG"::"L", 
+        "UAC"::"Y", "UAU"::"Y", "UAA"::"_", "UAG"::"_", 
+        "UGC"::"C", "UGU"::"C", "UGA"::"_", "UGG"::"W" )
+    
+    start_translation = findfirst("AUG", uppercase(rna_seq))[2]
+    triplets = 2
+    for (num, sequence) in enumerate(start_translation:length(rna_seq))
+        if num % 3 == 0 && num < length(rna_seq)
+            triplents = rna_seq[sequence:sequence + triplets]
+            println(triplents)
+            append!(traslation, table[triplents])
+        end
+    end
+    return join(traslation, "")
+end
